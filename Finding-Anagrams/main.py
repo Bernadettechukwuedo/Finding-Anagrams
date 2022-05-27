a86cad340feb80931a842e81ce13eb602dc4a88e
Bernadettechukwuedo <chukwuedoogochukwubernadette@gmail.com>
2022-05-27 10:13:52 +0100

    init

---
+++ Finding-Anagrams/main.py
@@ -0,0 +1,16 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+print(find_anagram("won", "now"))
+    return False
+
+def find_anagram(word, anagram):
+print(find_anagram("life", "nile"))
+    return False
+    
+
